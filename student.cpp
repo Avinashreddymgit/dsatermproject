593cbce294a1c1757b2d9926d20fc12e9936ca28
Avinashreddymgit <avinashreddymummasani@gmail.com>
2025-02-19 10:26:28 +0530

    Update student.cpp

--- student.cpp
+++ student.cpp
@@ -1,1 +1,9 @@
-
+#include "student.h"
+using namespace std;
+studentinfo:: studentinfo(int r,string na,float cg,int n,couseinfo* arr){
+  rollno=r;
+  name=na;
+  cgpa=cg;
+  numbersub=n;
+  course=arr;
+}
